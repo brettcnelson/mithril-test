6d794623ce0946b2bacf75b3aff0879c487d21af
Brett Nelson <brettcnelson@gmail.com>
2018-03-18 23:42:25 -0400

    debugs server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT;
+
 app.use(express.static('/dist'));
 
 
-app.listen(process.env.PORT, () => console.log(`express listening on ${port}`));
+app.listen(port, () => console.log(`express listening on ${port}`));
